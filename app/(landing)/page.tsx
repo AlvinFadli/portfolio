293e1796a4bb9d12235809fd5d9683db81f613c0
alvinfdl21 <alvinfdl21@gmail.com>
2024-10-25 22:18:08 +0700

    chore: remove unused imports

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,7 +2,6 @@ import { Container, Main, Section } from "@/components/craft";
 import Hero from "@/components/home-page/hero";
 import About from "@/components/home-page/about";
 import ProjectShowcase from "@/components/home-page/project-showcase";
-import BlogShowcase from "@/components/home-page/blog-showcase";
 import CTA from "@/components/home-page/cta";
 
 export default function HomePage() {
