7d5b8fc3f5a2f031826e040c46854347bb07ed78
alvinfdl21 <alvinfdl21@gmail.com>
2025-01-16 23:16:39 +0700

    chore: remove unused import and codes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,6 @@ import HomeButton from "@/components/home-button";
 import Footer from "@/components/layout/footer";
 
 import { PaperclipIcon } from "lucide-react";
-import { WorkExperience } from "@/data/work";
 import { Poor_Story } from "next/font/google";
 
 const poorStory = Poor_Story({ weight: "400", subsets: ["latin"] });
@@ -27,36 +26,8 @@ export default function AboutPage() {
                   About me, my <span>story</span> and my <span>experience</span>
                 </h2>
               </div>
-              {/* <div className="flex flex-col md:flex-row gap-10 mt-10">
-                <div className="w-5/6 md:w-2/5 mx-auto h-60 rounded-lg">
-                  <Image
-                    src={"/images/about/profile-2.webp"}
-                    alt="Profile"
-                    className="object-cover w-full h-full rounded-xl"
-                    width={500}
-                    height={500}
-                    priority
-                  />
-                </div>
-                <div className="flex-1 flex flex-col gap-5 justify-center text-center md:text-left">
-                  <p className="text-pretty">
-                    As a software engineer with one year of experience. I
-                    specialize in creating amazing website with modern
-                    technologies.
-                  </p>
-                  <div>
-                    <Button asChild>
-                      <Link href={"mailto:alvinfdl21@gmail.com"}>
-                        <MailIcon className="mr-2 h-4 w-4" />
-                        Get in touch
-                      </Link>
-                    </Button>
-                  </div>
-                </div>
-              </div> */}
               <div className="mt-8 text-pretty space-y-3">
                 <div className="w-4/5 sm:w-3/5 md:w-2/5 mx-auto md:float-right px-2 pt-2 pb-10 bg-slate-50 rounded-sm -rotate-3 md:ml-5 mb-10">
-                  {/* <div className="px-2 pt-2 pb-10 bg-slate-50 rounded-sm -rotate-3"> */}
                   <Image
                     src={"/images/about/profile-2.webp"}
                     alt="Profile"
@@ -138,57 +109,6 @@ export default function AboutPage() {
               </div>
             </div>
           </Container>
-          {/* <Container>
-            <div className="w-full md:w-[calc(100%+8rem)] md:-mx-16 mx-auto my-14 md:my-24 ">
-              <div className="flex flex-col md:flex-row w-full mx-auto items-center">
-                <div className="w-4/5 md:w-3/5 shadow-sm h-60 rounded-lg -rotate-6 md:-mr-10">
-                  <Image
-                    src={"/images/about/henkel.webp"}
-                    alt="Profile"
-                    className="object-cover w-full h-full rounded-xl"
-                    width={500}
-                    height={500}
-                  />
-                </div>
-                <div className="w-4/5 md:w-3/5 shadow-sm h-60 rounded-lg md:-mt-10 rotate-3">
-                  <Image
-                    src={"/images/about/indonesiannight.webp"}
-                    alt="Profile"
-                    className="object-cover w-full h-full rounded-xl"
-                    width={500}
-                    height={500}
-                  />
-                </div>
-                <div className="w-4/5 md:w-3/5 shadow-sm h-60 rounded-lg -rotate-6 md:-ml-10 md:mt-10">
-                  <Image
-                    src={"/images/about/techbros.webp"}
-                    alt="Profile"
-                    className="object-cover w-full h-full rounded-xl"
-                    width={500}
-                    height={500}
-                  />
-                </div>
-              </div>
-            </div>
-          </Container> */}
-          {/* <Container>
-            <div className="flex flex-col lg:flex-row gap-5 mt-8">
-              <p className="font-bold text-2xl w-2/5">Work Experience</p>
-              <div className="flex-1 flex flex-col space-y-4 divide-y">
-                {WorkExperience.map((item, index) => (
-                  <div className={`${index > 0 ? `pt-4` : `pt-0`}`} key={index}>
-                    <div>
-                      <h2 className="font-semibold">{item.title}</h2>
-                    </div>
-                    <div className="flex justify-between">
-                      <p className="text-sm">{item.company}</p>
-                      <p className="text-sm">{item.timespan}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </Container> */}
           <Container>
             <Footer />
           </Container>
