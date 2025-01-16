09ec76e2424a518df4468ca429151e59229c01ff
alvinfdl21 <alvinfdl21@gmail.com>
2025-01-16 23:13:03 +0700

    revamp(about): ui adjustment

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
@@ -6,8 +5,11 @@ import { Container, Main, Section } from "@/components/craft";
 import HomeButton from "@/components/home-button";
 import Footer from "@/components/layout/footer";
 
-import { MailIcon, PaperclipIcon } from "lucide-react";
+import { PaperclipIcon } from "lucide-react";
 import { WorkExperience } from "@/data/work";
+import { Poor_Story } from "next/font/google";
+
+const poorStory = Poor_Story({ weight: "400", subsets: ["latin"] });
 
 export default function AboutPage() {
   return (
@@ -25,7 +27,7 @@ export default function AboutPage() {
                   About me, my <span>story</span> and my <span>experience</span>
                 </h2>
               </div>
-              <div className="flex flex-col md:flex-row gap-10 mt-10">
+              {/* <div className="flex flex-col md:flex-row gap-10 mt-10">
                 <div className="w-5/6 md:w-2/5 mx-auto h-60 rounded-lg">
                   <Image
                     src={"/images/about/profile-2.webp"}
@@ -33,6 +35,7 @@ export default function AboutPage() {
                     className="object-cover w-full h-full rounded-xl"
                     width={500}
                     height={500}
+                    priority
                   />
                 </div>
                 <div className="flex-1 flex flex-col gap-5 justify-center text-center md:text-left">
@@ -50,8 +53,23 @@ export default function AboutPage() {
                     </Button>
                   </div>
                 </div>
-              </div>
+              </div> */}
               <div className="mt-8 text-pretty space-y-3">
+                <div className="w-4/5 sm:w-3/5 md:w-2/5 mx-auto md:float-right px-2 pt-2 pb-10 bg-slate-50 rounded-sm -rotate-3 md:ml-5 mb-10">
+                  {/* <div className="px-2 pt-2 pb-10 bg-slate-50 rounded-sm -rotate-3"> */}
+                  <Image
+                    src={"/images/about/profile-2.webp"}
+                    alt="Profile"
+                    className="object-cover w-full h-full rounded-t-sm"
+                    width={500}
+                    height={500}
+                    priority
+                  />
+                  <div className="absolute bottom-2 left-1/2 -translate-x-1/2 text-slate-800">
+                    <p className={poorStory.className}>Me!</p>
+                  </div>
+                  {/* </div> */}
+                </div>
                 <p className="">
                   My name is{" "}
                   <span className="text-primary-600">
@@ -120,7 +138,7 @@ export default function AboutPage() {
               </div>
             </div>
           </Container>
-          <Container>
+          {/* <Container>
             <div className="w-full md:w-[calc(100%+8rem)] md:-mx-16 mx-auto my-14 md:my-24 ">
               <div className="flex flex-col md:flex-row w-full mx-auto items-center">
                 <div className="w-4/5 md:w-3/5 shadow-sm h-60 rounded-lg -rotate-6 md:-mr-10">
@@ -152,8 +170,8 @@ export default function AboutPage() {
                 </div>
               </div>
             </div>
-          </Container>
-          <Container>
+          </Container> */}
+          {/* <Container>
             <div className="flex flex-col lg:flex-row gap-5 mt-8">
               <p className="font-bold text-2xl w-2/5">Work Experience</p>
               <div className="flex-1 flex flex-col space-y-4 divide-y">
@@ -170,7 +188,7 @@ export default function AboutPage() {
                 ))}
               </div>
             </div>
-          </Container>
+          </Container> */}
           <Container>
             <Footer />
           </Container>
